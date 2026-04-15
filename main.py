a88de1c303129275bfd64387e84e29c3abeeed89
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-15 22:11:00 +0200

    Add alternative model options in main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ MODEL_ID = "stabilityai/sdxl-turbo"
 # Alternative models:
 # MODEL_ID = "stabilityai/stable-diffusion-xl-base-1.0"
 # MODEL_ID = "runwayml/stable-diffusion-v1-5"
+# MODEL_ID = "black-forest-labs/FLUX.1-dev"
+# MODEL_ID = "mistralai/Mistral-7B-v0.1"
+# MODEL_ID = "meta-llama/Llama-2-7b-hf"
+# MODEL_ID = "gpt2"
+# MODEL_ID = "facebook/opt-350m"
+# MODEL_ID = "EleutherAI/gpt-neo-125M"
 
 PROMPT = "A futuristic cyberpunk city at night with neon lights, detailed, 4k, cinematic lighting, rain reflections, flying cars, holographic billboards, cyberpunk aesthetic"
 OUTPUT_FILE = "output.png"
@@ -16,6 +22,11 @@ MODEL_PIPELINES = {
     "stabilityai/stable-diffusion-xl-base-1.0": StableDiffusionXLPipeline,
     "black-forest-labs/FLUX.1-dev": DiffusionPipeline,
     "runwayml/stable-diffusion-v1-5": StableDiffusionPipeline,
+    "mistralai/Mistral-7B-v0.1": DiffusionPipeline,
+    "meta-llama/Llama-2-7b-hf": DiffusionPipeline,
+    "gpt2": DiffusionPipeline,
+    "facebook/opt-350m": DiffusionPipeline,
+    "EleutherAI/gpt-neo-125M": DiffusionPipeline,
 }
 
 # Select pipeline class
